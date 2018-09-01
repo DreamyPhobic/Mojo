be1f92d39881be79a58c53085b741f0ee0c71f50
tranquilizerTuffle <iit2017104@iiita.ac.in>
2018-09-01 15:28:20 +0100

    Fragments added

---
+++ app/src/main/java/com/mojostudios/mojopay/Page1Fragment.java
@@ -0,0 +1,19 @@
+package com.mojostudios.mojopay;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+    public class Page1Fragment extends Fragment {
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_page1, container, false);
+            return rootView;
+        }
+
+    }
+
